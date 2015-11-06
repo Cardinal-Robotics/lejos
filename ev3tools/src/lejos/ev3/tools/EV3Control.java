f0d3133077fff8dba49ce2b6dc4113413d2e7f99
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2015-11-06 11:37:38 +0000

    Remove stack dump for common errors

--- ev3tools/src/lejos/ev3/tools/EV3Control.java
+++ ev3tools/src/lejos/ev3/tools/EV3Control.java
@@ -15,7 +15,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.rmi.ConnectException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.Collection;
 
@@ -1552,6 +1554,10 @@ public class EV3Control implements ListSelectionListener, NXTProtocol, ConsoleVi
 				cvc.connectTo(name, name, 0, true);
 				if (row >= 0) updateConnectionStatus(row, EV3ConnectionState.CONNECTED);
 				showFiles();
+			} catch (NotBoundException e) {
+				System.err.println("RMI not running - check that menu is running and give it time to start RMI.");
+			} catch (ConnectException e) {
+				System.err.println("Connect failed - check that menu is running and give it time to start RMI.");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
